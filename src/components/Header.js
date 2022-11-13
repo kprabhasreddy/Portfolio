a2f37339274989af47db23555d977b0370f95bc6
Prabhas Reddy <kprabhasreddy@gmail.com>
2022-11-14 01:50:56 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ const Header = () => {
             <div className='header__content'>
                 <h1>Hi, I'm Prabhas Reddy</h1>
                 <p>Blockchain Developer</p>
+                <a href="https://docs.google.com/document/d/1jIX0tbvQntauTTF2LTgWfltqBSeMROv0fwMt5vtlEmg/edit?usp=sharing" className="button">Here's my Resume</a>
             </div>
         </section>
     );
